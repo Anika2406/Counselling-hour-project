5de30226643ba9c39b13a985fadeda3d925ec179
Anika2406 <70244192+Anika2406@users.noreply.github.com>
2020-08-27 00:59:35 +0600

    added html and css files for this project

---
+++ index.php
@@ -0,0 +1,184 @@
+		<html>
+		<head>
+			<title> css project </title>
+			 <link rel="stylesheet" type="text/css" href="main.css" >
+			 <link rel="stylesheet" type="text/css" href="fontawosem-all.css" >
+		</head>
+		<body>
+			<header>
+             
+				<div class="nav">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">about</a></li>
+						<li><a href="register.php">Registration</a></li>
+						<li><a href="">login</a>
+                            <ul>
+                                <li><a href="">Teacher</a></li>
+						        <li><a href="">Student</a></li> 
+	
+                             </ul>
+
+						</li>
+						<li><a href="">Search</a></li>
+						
+					</ul>
+				</div>
+				<div class="banner-cover" style="background-image: url(img/1.jpg);">
+					
+				<div class="banner-text">
+					<button type="submit">learn online class</button>
+					<button type="submit">Bridge course</button>
+					<button type="submit">Huge library</button>
+					
+				</div>
+			</div>
+			<div class="about-section">
+				<div class="title">
+					<h2>About us</h2>
+				</div>
+				<div class="about">
+					<div class="row"> 
+						<div class="col">
+					<div class="about-logo">
+						<img src="img/2.jpg"alt="amit">
+					</div>
+				</div>
+				<div class="col">
+					
+				
+					<div class="about-text">
+						<h2>
+							Daffodil International University
+						</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?
+						</p>
+					</div>
+				</div>
+				</div>
+				</div>
+			</div>
+			<div class="service-section">
+				<div class="title">
+					<h2>Our Service</h2>
+				</div>
+				<div class="service">
+					<div class="serv-icon">
+						<i class="fab fa-html5">
+							
+						</i>
+					</div>
+					<h3>
+						Web design
+					</h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?</p>
+				</div>
+				<div class="service">
+					<div class="serv-icon">
+						<i class="fab fa-html5">
+							
+						</i>
+					</div>
+					<h3>
+						Php
+					</h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?</p>
+				</div>
+				<div class="service">
+					<div class="serv-icon">
+						<i class="fab fa-html5">
+							
+						</i>
+					</div>
+					<h3>
+						Web design
+					</h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?</p>
+				</div>
+				<div class="service">
+					<div class="serv-icon">
+						<i class="fab fa-html5">
+							
+						</i>
+					</div>
+					<h3>
+						Web design
+					</h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?</p>
+				</div>
+				<div class="service">
+					<div class="serv-icon">
+						<i class="fab fa-html5">
+							
+						</i>
+					</div>
+					<h3>
+						Web design
+					</h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?</p>
+				</div>
+				<div class="service">
+					<div class="serv-icon">
+						<i class="fab fa-html5">
+							
+						</i>
+					</div>
+					<h3>
+						Web design
+					</h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium corporis, illum, rerum praesentium cupiditate inventore sed vero itaque impedit, recusandae eaque. Eligendi harum, molestias ducimus eum quas ut officia sit?</p>
+				</div>
+			</div>
+			<div class="contat-section">
+				<div class="title">
+					<h2>contact us</h2>
+				</div>
+				<div class="contact">
+					<div class="cont-icon">
+						<i class="fas fa-phone"></i>
+					</div>
+					<div class="cont-text">
+						<p> +8801956945283</p>
+						<p> +8801956945283</p>
+					</div>
+				</div>
+				<div class="contact">
+					<div class="cont-icon">
+						<i class="fas fa-phone"></i>
+					</div>
+					<div class="cont-text">
+						<p> +8801956945283</p>
+						<p> +8801956945283</p>
+					</div>
+				</div>
+				<div class="contact">
+					<div class="cont-icon">
+						<i class="fas fa-phone"></i>
+					</div>
+					<div class="cont-text">
+						<p> +8801956945283</p>
+						<p> +8801956945283</p> 
+					</div>
+				</div>
+				<form >
+					<div class="contact-form">
+						<div class="input-tag"><input type="text" placeholder="your name"></div>
+						<div class="input-tag"><input type="email" placeholder="your email"></div>
+						<div class="input-tag"><input type="text" placeholder="your subject"></div>
+						<div class="input-tag"><textarea rows="20" cols="30"></textarea></div>
+						<div class="input-tag"><button class="sudmit">submit</button></div>
+							
+						</div>
+					</div>
+				</form>
+			</div>
+			</header>
+			<footer>
+				<div class="fotter-tag">
+					       <p>Copyright &copy; 2020, Amit Chowdhury</p>
+
+				</div>
+            </footer>
+				</body>
+				</html>
